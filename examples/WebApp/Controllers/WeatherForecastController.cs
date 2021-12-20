aef2f07caea3d246f102cf7153a80b96ae89c76d
Andrew <andrew.gubskiy@outlook.com>
2021-12-20 13:52:06 +0200

    Add more logs

--- examples/WebApp/Controllers/WeatherForecastController.cs
+++ examples/WebApp/Controllers/WeatherForecastController.cs
@@ -31,7 +31,12 @@ namespace WebApp.Controllers
             var rng = new Random();
             var temperatureC = rng.Next(-20, 55);
             
-            _logger.LogInformation($"Temperature: {temperatureC}");
+            _logger.LogTrace($"Trace Temperature: {temperatureC}");
+            _logger.LogDebug($"Debug Temperature: {temperatureC}");
+            _logger.LogInformation($"Information Temperature: {temperatureC}");
+            _logger.LogWarning($"Warning Temperature: {temperatureC}");
+            _logger.LogError($"Error Temperature: {temperatureC}");
+            _logger.LogCritical($"Critical Temperature: {temperatureC}");
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
