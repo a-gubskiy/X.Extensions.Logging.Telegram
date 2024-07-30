fea777c6c2d7900ad9c911a3face38ad2221004a
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-30 19:03:46 +0300

    Remove secrets

--- examples/Example.Core/ExampleAppSettings.cs
+++ examples/Example.Core/ExampleAppSettings.cs
@@ -2,6 +2,6 @@
 
 public class ExampleAppSettings
 {
-    public const string Token = "5357457300:AAGrINxccN3hhBwhz9db_84rcQx6Y2edI88";
-    public const string ChatId = "-1002209155985";
+    public const string Token = "";
+    public const string ChatId = "";
 }
