a5add0d9ea8590a18f0a58c403bd80af00eed868
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-30 19:08:22 +0300

    Update TelegramLogWriter

--- src/X.Extensions.Logging.Telegram.Base/TelegramLogWriter.cs
+++ src/X.Extensions.Logging.Telegram.Base/TelegramLogWriter.cs
@@ -44,6 +44,10 @@ public class TelegramLogWriter : ILogWriter
             parseMode: ParseMode.Html,
             cancellationToken: cancellationToken);
 
+        #if DEBUG
+        
         Trace.WriteLine(result.MessageId);
+            
+        #endif
     }
 }
