b2681937906ef53e86ae23273aa662490dcc64ef
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-30 14:36:13 +0300

    Fix reference

--- examples/Example.Core/ExampleAppSettings.cs
+++ examples/Example.Core/ExampleAppSettings.cs
@@ -2,6 +2,6 @@
 
 public class ExampleAppSettings
 {
-    public const string Token = "00000000:000000000-00000000-000000";
-    public const string ChatId = "-0000000000000";
+    public const string Token = "5357457300:AAFao32RfqfZ5-6IsZOK1Of3-gPTXBGHK1k";
+    public const string ChatId = "-1002209155985";
 }
